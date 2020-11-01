420411e579e521700da81de099ae6b3d309cc9d5
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2020-11-01 16:23:19 +0530

    update axios-local.js

--- client/src/axios-local.js
+++ client/src/axios-local.js
@@ -3,7 +3,7 @@ import store from './store/store';
 import { logout } from './store/actions';
 
 const instance = axios.create({
-	baseURL: `${process.env.REACT_APP_BASE_URL || 'http://localhost:5000'}/api`
+	baseURL: `${process.env.REACT_APP_BASE_URL}/api`
 });
 
 instance.interceptors.response.use(
