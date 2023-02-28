c76e8ea4e6833d84eb3ee4a098098ef65591bd9f
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2023-02-28 12:19:56 +0530

    update backend code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,22 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+// const config = require('config');
+require("dotenv").config();
 
-const HttpError = require('../models/Http-error');
+const HttpError = require("../models/Http-error");
 
 module.exports = (req, res, next) => {
-	const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
-	if (!token) {
-		return next(new HttpError('Access Denied', 401));
-	}
+  if (!token) {
+    return next(new HttpError("Access Denied", 401));
+  }
 
-	try {
-		const decodedPayload = jwt.verify(token, config.get('jwtSecret'));
+  try {
+    const decodedPayload = jwt.verify(token, process.env.jwtSecret);
 
-		req.user = decodedPayload.user;
-		next();
-	} catch (err) {
-		next(new HttpError('Invalid Token', 401));
-	}
+    req.user = decodedPayload.user;
+    next();
+  } catch (err) {
+    next(new HttpError("Invalid Token", 401));
+  }
 };
